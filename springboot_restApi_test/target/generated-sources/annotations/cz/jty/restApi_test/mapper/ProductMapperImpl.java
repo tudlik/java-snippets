0fb9ff9acb9c03198a27cc421b7db5ef0aa24b43
Jan Tylecek <jan.tylecek@ness.com>
2024-02-19 08:58:30 +0100

    added comments

--- springboot_restApi_test/target/generated-sources/annotations/cz/jty/restApi_test/mapper/ProductMapperImpl.java
+++ springboot_restApi_test/target/generated-sources/annotations/cz/jty/restApi_test/mapper/ProductMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-18T17:17:30+0100",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Amazon.com Inc.)"
+    date = "2024-02-19T08:42:52+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Amazon.com Inc.)"
 )
 @Component
 public class ProductMapperImpl implements ProductMapper {
